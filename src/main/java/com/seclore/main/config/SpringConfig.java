0cc520110285bd0a83749a37f295a1bfba7e5581
Agastya Singh <72365639+AgastyaSinghh@users.noreply.github.com>
2023-09-08 16:55:11 +0530

    Updated files
    Added GlobalExceptionHandler
    Added comments for TodoController, UserController, ViewController

--- src/main/java/com/seclore/main/config/SpringConfig.java
+++ src/main/java/com/seclore/main/config/SpringConfig.java
@@ -1,7 +1,6 @@
 package com.seclore.main.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
--- src/main/java/com/seclore/main/controller/rest/TodoController.java
+++ src/main/java/com/seclore/main/controller/rest/TodoController.java
@@ -18,6 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 import com.seclore.main.domain.Todo;
 import com.seclore.main.service.Todo.TodoServiceInterface;
 
+
+/**
+ * The TodoController class is responsible for handling HTTP requests related to managing TODOs.
+ * It provides endpoints for retrieving all TODOs, creating a new TODO, retrieving a TODO by ID,
+ * updating a TODO by ID, and deleting a TODO by ID.
+ * It requires user's bearer token to be passed in the authorization headers.
+ *
+ * @RestController Indicates that this class is a controller for handling RESTful HTTP requests.
+ * @RequestMapping("/api/todo") Specifies the base URL path for mapping this controller's endpoints.
+ */
 @RestController
 @RequestMapping("/api/todo")
 public class TodoController {
@@ -25,12 +35,29 @@ public class TodoController {
 	@Autowired
 	private TodoServiceInterface todoService;
 
+    /**
+     * Retrieve all TODOs associated with a specific user.
+     * The userId is extracted from the token and passed in through request attribute
+     *
+     * @param userId The user ID for whom to retrieve TODOs.
+     * @return ResponseEntity containing a list of TODOs if successful (HTTP 200 OK),
+     *         or an error response if retrieval fails (HTTP 500 Internal Server Error).
+     */
 	@GetMapping("")
 	ResponseEntity<List<Todo>> getAllTodoByUserId(@RequestAttribute("userId") int userId) {
 		List<Todo> todoList = todoService.getTodoListByUserId(userId);
 		return new ResponseEntity<List<Todo>>(todoList,  HttpStatus.OK);
 	}
 
+	/**
+     * Create a new TODO for a specific user.
+     *
+     * @param todo   The Todo object containing TODO information to be created.
+     * @param userId The user ID for whom to create the TODO; The userId is 
+     * 		   extracted from the token and passed in through request attribute
+     * @return ResponseEntity with a success message if TODO creation is successful (HTTP 201 Created),
+     *         or an error message if creation fails (HTTP 304 Not Modified).
+     */
 	@PostMapping("")
 	ResponseEntity<String> createTodo(@RequestBody Todo todo, @RequestAttribute("userId") int userId) {
 		boolean success = todoService.createTodo(todo, userId);
@@ -39,12 +66,27 @@ public class TodoController {
 		return new ResponseEntity<String>("Failed to create todo!", HttpStatus.NOT_MODIFIED);
 	}
 
+	/**
+     * Retrieve a TODO by its unique ID.
+     *
+     * @param todoId The unique ID of the TODO to retrieve.
+     * @return ResponseEntity containing the TODO if found (HTTP 200 OK),
+     *         or an error response if retrieval fails (HTTP 500 Internal Server Error).
+     */
 	@GetMapping("/{id}")
 	ResponseEntity<Todo> getTodoById(@PathVariable(name = "id") int todoId) {
 		Todo todo = todoService.getTodoById(todoId);
 		return new ResponseEntity<Todo>(todo, HttpStatus.OK);
 	}
 
+	/**
+     * Update a TODO by its unique ID.
+     *
+     * @param todo The Todo object containing updated TODO information{title:string, description: string, status:{"In Progress", "Completed", "Pending"}}.
+     * @param id   The unique ID of the TODO to update.
+     * @return ResponseEntity with a success message if TODO update is successful (HTTP 200 OK),
+     *         or an error message if update fails (HTTP 304 Not Modified).
+     */
 	@PutMapping("/{id}")
 	ResponseEntity<String> updateTodoById(@RequestBody Todo todo, @PathVariable int id) {
 		todo.setId(id);
@@ -54,6 +96,13 @@ public class TodoController {
 		return new ResponseEntity<String>("Failed to edit todo!", HttpStatus.NOT_MODIFIED);
 	}
 
+	/**
+     * Delete a TODO by its unique ID.
+     *
+     * @param todoId The unique ID of the TODO to delete.
+     * @return ResponseEntity with a success message if TODO deletion is successful (HTTP 200 OK),
+     *         or an error message if deletion fails (HTTP 304 Not Modified).
+     */
 	@DeleteMapping("/{id}")
 	ResponseEntity<String> deleteTodoById(@PathVariable(name = "id") int todoId) {
 		boolean success = todoService.deleteTodo(todoId);
